0f428a064cf060db5c02c2ecae9bfa217f8c2048
Amit-Yadav56 <139137290+Amit-Yadav56@users.noreply.github.com>
2023-10-27 15:31:21 +0530

    server created

--- ai.js
+++ ai.js
@@ -1,19 +1,23 @@
-import OpenAI from 'openai';
-
+import OpenAI from "openai";
+// const openai = new OpenAI({
+//   apiKey: process.env.Api_key,
+// });
 const openai = new OpenAI({
-  apiKey: ""
+  apiKey: "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"
 });
 
 export const openfun=async(value)=>{
 const chatCompletion = await openai.chat.completions.create({
-    model: "gpt-2",
+    model: "gpt-3.5-turbo",
     messages: [{"role": "user", "content": `Create short story about ${value}`}],
     max_tokens:20
   });
   return (chatCompletion.choices[0]?.message?.content);
 }
-openfun();
-function req(){
-  const val=document.getElementById('prom').value;
-  document.querySelector('.Story').innerHTML=openfun(val);
-}
+// openfun();
+// function req(){
+//   const val=document.getElementById('prom').value;
+//   document.querySelector('.Story').innerHTML=openfun(val);
+// }
+  
+
